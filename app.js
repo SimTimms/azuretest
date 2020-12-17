cce22eaccf85983d2e1ffbca5b57fe2fd415158d
Tim.Simms <tim-simms@hotmail.com>
2020-12-17 14:44:04 +0000

    Save

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 });
 
 app.post('/', (req, res) => {
-  console.log(req.body);
+  console.log(req);
   var validationEventType = 'Microsoft.EventGrid.SubscriptionValidationEvent';
 
   for (var events in req.body) {
