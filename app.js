bcb94b7742cd3e1438c730b6697228e2fcbf6246
Tim.Simms <tim-simms@hotmail.com>
2020-12-17 14:46:57 +0000

    Save

--- app.js
+++ app.js
@@ -1,33 +1,30 @@
 const express = require('express');
 const app = express();
+var bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World, from express');
-});
+app.use(bodyParser.json());
 
 app.post('/', (req, res) => {
-  console.log(req);
-  var validationEventType = 'Microsoft.EventGrid.SubscriptionValidationEvent';
-
-  for (var events in req.body) {
-    var body = req.body[events];
-    // Deserialize the event data into the appropriate type based on event type
-    if (body.data && body.eventType == validationEventType) {
-      context.log(
-        'Got SubscriptionValidation event data, validation code: ' +
-          body.data.validationCode +
-          ' topic: ' +
-          body.topic
-      );
-
-      // Do any additional validation (as required) and then return back the below response
-      var code = body.data.validationCode;
-      context.res = { status: 200, body: { ValidationResponse: code } };
+  var header = req.get('Aeg-Event-Type');
+  if (header && header === 'SubscriptionValidation') {
+    var event = req.body[0];
+    var isValidationEvent =
+      event &&
+      event.data &&
+      event.data.validationCode &&
+      event.eventType &&
+      event.eventType == 'Microsoft.EventGrid.SubscriptionValidationEvent';
+    if (isValidationEvent) {
+      return res.send({
+        validationResponse: event.data.validationCode,
+      });
     }
   }
+
+  // Do something on other event types
+  console.log(req.body);
+  res.send(req.body);
 });
 
-app.listen(port, () =>
-  console.log(`Hello world app listening on port ${port}!`)
-);
+app.listen(port, () => console.log('Example app listening on port 3000!'));
