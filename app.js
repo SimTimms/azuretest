e30ea56b9addea8f970bbf68ac0eb7e1c3e4c694
Tim.Simms <tim-simms@hotmail.com>
2020-12-17 14:14:31 +0000

    push

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World, from express');
+  var validationEventType = 'Microsoft.EventGrid.SubscriptionValidationEvent';
+
+  for (var events in req.body) {
+    var body = req.body[events];
+    // Deserialize the event data into the appropriate type based on event type
+    if (body.data && body.eventType == validationEventType) {
+      context.log(
+        'Got SubscriptionValidation event data, validation code: ' +
+          body.data.validationCode +
+          ' topic: ' +
+          body.topic
+      );
+
+      // Do any additional validation (as required) and then return back the below response
+      var code = body.data.validationCode;
+      context.res = { status: 200, body: { ValidationResponse: code } };
+    }
+  }
 });
 
 app.listen(port, () =>
